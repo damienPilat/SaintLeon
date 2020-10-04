d416e4dc857a6d39ac1a7aa6ef3624f8ea5e0150
damienPilat <dspilat@hotmail.com>
2020-10-04 15:21:30 +0200

    Specify db entries for Index

--- stLeon/views.py
+++ stLeon/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_list_or_404
+from .models import Actualite, Anoter, Bienvenue
 
 
 def index(request):
-    return render(request, 'stLeon/main.html')
+    actualite = get_list_or_404(Actualite)
+    aNoter = get_list_or_404(Anoter)
+    context = {'actualite': actualite[:3],      # Return first X amount
+               'aNoter': aNoter[3:]}            # !Limit to latest (5)!
+    return render(request, 'stLeon/main.html', context)
 
