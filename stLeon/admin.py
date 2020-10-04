02c7c9a440d00f555b50b9c4edabf44a96c376c3
damienPilat <dspilat@hotmail.com>
2020-10-04 15:21:00 +0200

    New classes (Actualite, Bienvenue)

--- stLeon/admin.py
+++ stLeon/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin        # Exterior Library
-from .models import Batiments           # Project Models
+from .models import Actualite, Anoter, Bienvenue, Batiments          # Project Models
 
+admin.site.register(Actualite)
+admin.site.register(Anoter)
+admin.site.register(Bienvenue)
 admin.site.register(Batiments)
--- stLeon/models.py
+++ stLeon/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 
 
+# List des Actualités
+class Actualite(models.Model):
+    titre = models.CharField(max_length=500)
+    text = models.TextField(max_length=5000)
+    date = models.DateTimeField('date ajouter')
+    link = models.CharField(max_length=2048)
+    first_element = models.BooleanField(default=False)
+    last_element = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titre
+
+
+# List des elements À Noter
+class Anoter(models.Model):
+    titre = models.CharField(max_length=500)
+    text = models.TextField(max_length=5000)
+    date = models.DateTimeField('date ajouter')
+    link = models.CharField(max_length=2048)
+    img_src = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.titre
+
+
+# Elements de Bienvenue
+class Bienvenue(models.Model):
+    text = models.TextField(max_length=5000)
+    img_src = models.CharField(max_length=500, blank=True, default=None)
+
+
+# List des Batiments à Saint-Léon
 class Batiments(models.Model):
     nom = models.CharField(max_length=500)
     sousol = models.CharField(max_length=500)
