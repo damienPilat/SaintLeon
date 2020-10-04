8937f3be588115ec2163bbd6f9b5eca29561e5d9
damienPilat <dspilat@hotmail.com>
2020-10-04 17:00:58 +0200

    Support for Bienvenue elements

--- stLeon/views.py
+++ stLeon/views.py
@@ -5,7 +5,10 @@ from .models import Actualite, Anoter, Bienvenue
 def index(request):
     actualite = get_list_or_404(Actualite)
     aNoter = get_list_or_404(Anoter)
+    bienvenue = get_list_or_404(Bienvenue)
     context = {'actualite': actualite[:3],      # Return first X amount
-               'aNoter': aNoter[3:]}            # !Limit to latest (5)!
+               'aNoter': aNoter[3:],            # !Limit to latest (3)!
+               'bienvenue': bienvenue
+               }
     return render(request, 'stLeon/main.html', context)
 
