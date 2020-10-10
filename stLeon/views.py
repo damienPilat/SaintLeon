a2b1120700bf68c245e06969e6b8e138182b1b9e
damienPilat <dspilat@hotmail.com>
2020-10-10 11:37:14 +0200

    Test elements

--- stLeon/views.py
+++ stLeon/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render, get_list_or_404
-from .models import Actualite, Anoter, Bienvenue
+from .models import Actualite, Anoter, Bienvenue, Batiments
 
 
 def index(request):
     actualite = get_list_or_404(Actualite)
     aNoter = get_list_or_404(Anoter)
     bienvenue = get_list_or_404(Bienvenue)
+    horairesMesse = getHorairesMesse()
     context = {'actualite': actualite[:3],      # Return first X amount
-               'aNoter': aNoter[3:],            # !Limit to latest (3)!
-               'bienvenue': bienvenue
+               'aNoter': aNoter,            # !Limit to latest (3)!
+               'bienvenue': bienvenue,
+               'horairesMesse': horairesMesse
                }
     return render(request, 'stLeon/main.html', context)
 
+
+def test(request):
+    bienvenue = get_list_or_404(Bienvenue)
+    aNoter = get_list_or_404(Anoter)
+    horairesMesse = getHorairesMesse()
+    context = {'bienvenue': bienvenue,
+               'aNoter': aNoter,
+               'horairesMesse': horairesMesse}
+    return render(request, 'stLeon/test.html', context)
+
+
+def test_2(request):
+    batiments = get_list_or_404(Batiments)
+    context = {'batiments': batiments}
+    return render(request, 'stLeon/test_2.html', context)
+
+
+def getHorairesMesse():
+    return 'stLeon/images/horaires_messe_HorsVacances.png'
