19cf6a90cc216f14ed94a6089d9d689b85c054e4
damienPilat <dspilat@hotmail.com>
2020-10-10 11:37:21 +0200

    Test support

--- stLeon/urls.py
+++ stLeon/urls.py
@@ -3,5 +3,7 @@ from . import views             # Local functions
 
 app_name = 'stLeon'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('test', views.test, name='test'),
+    path('test_2', views.test_2, name="test_2")
 ]
